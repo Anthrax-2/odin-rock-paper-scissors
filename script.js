276abdf8fd99dded7304fb68f197f1321c4cb48d
Amer <203231960+Anthrax-2@users.noreply.github.com>
2026-08-13 12:54:02 +0300

    Add one round game logic; function 'playRound()'

--- script.js
+++ script.js
@@ -1,30 +1,67 @@
-const validPicks = ["rock", "paper", "scissors"];
+"use strict"
 
-playerScore = 0;
-computerScore = 0;
+const validPicks = ["rock", "paper", "scissors"]
 
+let playerScore = 0
+let computerScore = 0
+
+function playRound(playerPick, computerPick) {
+    if (playerPick === "scissors") {
+        if (computerPick === "paper") {
+            return `Player chose scissors. Computer chose paper. Player wins.\n`
+        }
+        else if (computerPick === "rock") {
+            return `Player chose scissors. Computer chose rock. Computer wins.\n`
+        }
+        else {
+            return `Player chose scissors. Computer chose scissors. Draw.\n`
+        }
+    }
+    else if (playerPick === "rock") {
+        if (computerPick === "scissors") {
+            return `Player chose rock. Computer chose scissors. Player wins.\n`
+        }
+        else if (computerPick === "paper") {
+            return `Player chose rock. Computer chose paper. Computer wins.\n`
+        }
+        else {
+            return `Player chose rock. Computer chose rock. Draw.\n`
+        }
+    }
+    else {
+        if (computerPick === "rock") {
+            return `Player chose paper. Computer chose rock. Player wins.\n`
+        }
+        else if (computerPick === "scissors") {
+            return `Player chose paper. Computer chose scissors. Computer wins.\n`
+        }
+        else {
+            return `Player chose paper. Computer chose paper. Draw.\n`
+        }
+    }
+
+}
 
 function validatePick(pick) {
     if (pick) {
         if (validPicks.includes(pick.toLowerCase())) {
-            return true;
+            return true
         }
-        return false;
+        return false
     }
-    return false;
+    return false
 }
 
 function getPlayerPick() {
-    let pick = prompt("Choose one of these (Rock, Paper, Scissors):");
+    let pick = prompt("Choose one of these (Rock, Paper, Scissors):")
     if (validatePick(pick)) {
-        return pick.toLowerCase();
+        return pick.toLowerCase()
     }
-    return null;
+    return null
 }
 
 function getComputerPick() {
-    let i = Math.floor(Math.random() * validPicks.length);
-    return validPicks[i];
+    let i = Math.floor(Math.random() * validPicks.length)
+    return validPicks[i]
 }
-
 
