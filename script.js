dcbcea57d6f7ae75d6af6423781430823eeffa95
Amer <203231960+Anthrax-2@users.noreply.github.com>
2026-08-13 11:44:57 +0300

    Add player input hndling and validation

---
+++ script.js
@@ -0,0 +1,19 @@
+const validPicks = ["rock", "paper", "scissors"];
+
+function validatePick(pick) {
+    if (pick) {
+        if (validPicks.includes(pick.toLowerCase())) {
+            return true;
+        }
+        return false;
+    }
+    return false;
+}
+
+function getPlayerPick() {
+    let pick = prompt("Choose one of these (Rock, Paper, Scissors):");
+    if (validatePick(pick)) {
+        return pick.toLowerCase();
+    }
+    return null;
+}
