434ee60d451494b94a59e131003f2e7e94b7fb3c
Amer <203231960+Anthrax-2@users.noreply.github.com>
2026-08-22 17:18:22 +0300

    Initialized variables in 'script.js'

--- script.js
+++ script.js
@@ -1,87 +1,13 @@
 "use strict"
 
-const validPicks = ["rock", "paper", "scissors"]
+const numberOfRounds = document.querySelector("#number-of-rounds")
 
-function playGame(numberOfRounds = 3) {
+const rock = document.querySelector("#rock")
+const paper = document.querySelector("#paper")
+const scissors = document.querySelector("scissors")
 
-    let playerScore = 0
-    let computerScore = 0
-
-    for (let i = 0; i < numberOfRounds; i++) {
-        let [message, winner] = playRound(getPlayerPick(), getComputerPick())
-        if (winner === `player`) {
-            playerScore++
-        }
-        else if (winner === `computer`) {
-            computerScore++
-        }
-        console.log(message)
-    }
-    if (playerScore > computerScore) {
-        return `Final score:\nPlayer: ${playerScore}\nComputer: ${computerScore}\nPlayer wins.`
-    } else if (playerScore < computerScore) {
-        return `Final score:\nPlayer: ${playerScore}\nComputer: ${computerScore}\nComputer wins.`
-    } else {
-        return `Final score:\nPlayer: ${playerScore}\nComputer: ${computerScore}\nDraw.`
-    }
-}
-
-function playRound(playerPick, computerPick) {
-    if (playerPick === "scissors") {
-        if (computerPick === "paper") {
-            return [`Player chose scissors. Computer chose paper. Player wins round.\n`, `player`]
-        } else if (computerPick === "rock") {
-            return [`Player chose scissors. Computer chose rock. Computer wins round.\n`, `computer`]
-        } else {
-            return [`Player chose scissors. Computer chose scissors. Draw.\n`, `draw`]
-        }
-    } else if (playerPick === "rock") {
-        if (computerPick === "scissors") {
-            return [`Player chose rock. Computer chose scissors. Player wins round.\n`, `player`]
-        } else if (computerPick === "paper") {
-            
-            return [`Player chose rock. Computer chose paper. Computer wins round.\n`, `computer`]
-        } else {
-            return [`Player chose rock. Computer chose rock. Draw.\n`, `draw`]
-        }
-    } else {
-        if (computerPick === "rock") {
-           
-            return [`Player chose paper. Computer chose rock. Player wins round.\n`, `player`]
-        } else if (computerPick === "scissors") {
-    
-            return [`Player chose paper. Computer chose scissors. Computer wins round.\n`, `computer`]
-        } else {
-            return [`Player chose paper. Computer chose paper. Draw.\n`, `draw`]
-        }
-    }
-}
-
-function validatePick(pick) {
-    if (pick) {
-        if (validPicks.includes(pick.toLowerCase())) {
-            return true
-        }
-        return false
-    }
-    return false
-}
-
-function getPlayerPick() {
-    let pick = prompt("Choose one of these (Rock, Paper, Scissors):")
-    if (validatePick(pick)) {
-        return pick.toLowerCase()
-    }
-    alert("Invalid input.")
-    return getPlayerPick()
-}
-
-function getComputerPick() {
-    let i = Math.floor(Math.random() * validPicks.length)
-    return validPicks[i]
-}
-
-console.log(playGame())
-
+const playerScore = document.querySelector("#player-score")
+const computerScore = document.querySelector("#computer-score")
+const finalScore = document.querySelector("final-score")
 
 
